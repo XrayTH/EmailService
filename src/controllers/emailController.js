895920e86e3bcc448dc3f0db6086e8262b999e09
XrayTH <143976649+XrayTH@users.noreply.github.com>
2024-11-02 16:19:18 -0500

    v2

--- src/controllers/emailController.js
+++ src/controllers/emailController.js
@@ -2,30 +2,42 @@ const nodemailer = require('nodemailer');
 const path = require('path');
 
 const sendEmail = async (req, res) => {
-  const { emails, subject, content } = req.body;
-  const files = req.files; // Cambia el acceso a archivos
-
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
-
-  const attachments = files ? files.map(file => ({
-    path: path.join(__dirname, '../uploads', file.filename)
-  })) : [];
-
-  const mailOptions = {
-    from: process.env.EMAIL_USER,
-    to: emails.join(','),
-    subject: subject,
-    text: content,
-    attachments: attachments,
-  };
-
   try {
+    let emails = req.body.emails;
+    if (typeof emails === 'string') {
+      emails = JSON.parse(emails);
+    }
+
+    if (!Array.isArray(emails)) {
+      return res.status(400).send('El campo "emails" debe ser un array');
+    }
+
+    const { subject, content } = req.body;
+    const files = req.files;
+
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+      timeout: 50000, 
+      connectionTimeout: 50000, 
+    });
+
+    const attachments = files ? files.map(file => ({
+      filename: file.originalname,
+      path: path.join(__dirname, '../uploads', file.filename)
+    })) : [];
+
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
+      to: emails.join(','),
+      subject: subject,
+      text: content,
+      attachments: attachments,
+    };
+
     await transporter.sendMail(mailOptions);
     res.status(200).send('Correos enviados con éxito');
   } catch (error) {
@@ -35,4 +47,3 @@ const sendEmail = async (req, res) => {
 };
 
 module.exports = { sendEmail };
-
